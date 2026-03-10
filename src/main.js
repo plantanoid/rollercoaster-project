6e3c53cb4d79196b342a75b461b681f97f1f1ee1
plantanoid <coenvdavis@gmail.com>
2026-03-09 19:34:10 -0500

    added an intro & an intro jingle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './style.css';
 
+// intro screen
+const introScreen = document.querySelector('#intro-screen');
+const startBtn = document.querySelector('#start-btn');
+const appContainer = document.querySelector('#app');
+
 // button stuff
 const counterDisplay = document.querySelector('#counter-display');
 const actionBtn = document.querySelector('#action-btn');
@@ -7,9 +12,31 @@ const actionBtn = document.querySelector('#action-btn');
 // sfx
 const hoverSfx = document.querySelector('#hover-sfx');
 const clickSfx = document.querySelector('#click-sfx');
+const introJingleSfx = document.querySelector('#intro-jingle-sfx');
 
-hoverSfx.volume = 1;
-clickSfx.volume = 1;
+if (hoverSfx && clickSfx) {
+  hoverSfx.volume = 0.2;
+  clickSfx.volume = 0.6;
+  introJingleSfx.volume = 0.6;
+}
+
+if (startBtn && introScreen && appContainer) {
+  startBtn.addEventListener('click', () => {
+    clickSfx.currentTime = 0;
+    introJingleSfx.currentTime = 0;
+    clickSfx.play();
+    introJingleSfx.play();
+
+    introScreen.classList.add('intro-fade-out');
+    appContainer.classList.add('app-visible');
+    appContainer.classList.remove('app-hidden');
+
+    // cleanup dom after aniamtion
+    setTimeout(() => {
+      introScreen.remove();
+    }, 600);
+  });
+}
 
 if (actionBtn && counterDisplay) {
   let count = 0;
@@ -19,8 +46,7 @@ if (actionBtn && counterDisplay) {
     hoverSfx.currentTime = 0;
     hoverSfx.play();
 
-    const randomDegrees = Math.floor(Math.random() *16 ) -7.5;
-
+    const randomDegrees = Math.floor(Math.random() *8.5 ) -3.75;
     actionBtn.style.setProperty('--random-rotation', `${randomDegrees}deg`);
   });
 
